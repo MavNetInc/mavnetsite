bcada1683020f91b6c234d076ec83f05aafe2fc1
MavNetInc <landonsnyder09@gmail.com>
2026-02-20 17:57:57 -0500

    Create events.js

---
+++ events.js
@@ -0,0 +1,6 @@
+// ===== MavNet Special Events =====
+const specialEvents = {
+  "2026-02-23": { message:"Presidents Day — No School" },
+  "2026-03-05": { message:"2-Hour Delay ⏰" },
+  "2026-03-12": { message:"Early Release — 12:30 PM" }
+};
